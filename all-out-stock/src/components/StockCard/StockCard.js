69e1ef6563ad494a61c734648aadaf1660a30daa
Anthony Chan <achan437@my.bcit.ca>
2021-03-30 15:02:39 -0700

    using stockcard for display

--- all-out-stock/src/components/StockCard/StockCard.js
+++ all-out-stock/src/components/StockCard/StockCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
 
-export default function StockCard({ stock }) {
+export default function StockCard({ company, average_price }) {
     return (
         <View style={styles.root}>
-            <Text style={styles.company}>{stock.company}</Text>
-            <Text>{`Average Price: $${stock.average_price}`}</Text>
+            <Text style={styles.company}>{company}</Text>
+            <Text>{`Average Price: $${average_price}`}</Text>
         </View>
     );
 }
--- all-out-stock/src/screens/StockScreen/StockScreen.js
+++ all-out-stock/src/screens/StockScreen/StockScreen.js
@@ -1,24 +1,22 @@
 import React, {useState,useEffect} from 'react'
-import { Text, TextInput, View } from 'react-native'
+import { Text, View } from 'react-native'
 import { finnhubClient } from '../../finnhub/config'
 import SearchBar from '../../components/SearchBar/SearchBar';
+import StockCard from '../../components/StockCard/StockCard'
 
 export default function StockScreen(props) {
     const [symbol, setSymbol] = useState("")
-    const [quote, setQuote] = useState({})
+    const [quote, setQuote] = useState()
+    const [name, setName] = useState("")
     
     const searchApi = () => {
         finnhubClient.quote(symbol, (error, data, response) => {
             setQuote(data)
         })
+        finnhubClient.companyProfile2({'symbol': symbol}, (error, data, response) => {
+            setName(data.name)
+        })
     }
-
-    useEffect(()=>{
-        searchApi()
-    },[]) 
-
-    console.log(symbol)
-    console.log(quote)
 
     return (
         <View>
@@ -26,11 +24,14 @@ export default function StockScreen(props) {
                 term={symbol} 
                 onTermChange={(text) => setSymbol(text)}
                 onTermSubmit={() => searchApi()} />
-            <Text>Stock Symbol: {symbol}</Text>
             {
                 quote &&
-                <Text>Current Price: {quote.c}</Text>
+                <StockCard
+                    company={name}
+                    average_price={(quote.h + quote.l)/2}
+                />
             }
+            
         </View>
     )
 }
