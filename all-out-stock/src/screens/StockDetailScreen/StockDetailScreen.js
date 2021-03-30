8dbce4d73ee7db8280f91772062a82dda65734ea
Steven <clai86@my.bcit.ca>
2021-03-30 14:14:12 -0700

    Stock detail page partially added

--- all-out-stock/src/screens/StockDetailScreen/StockDetailScreen.js
+++ all-out-stock/src/screens/StockDetailScreen/StockDetailScreen.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 
 export default function StockDetailScreen({ route }) {
-    const [stock, setStock] = useState({});
-
     return (
         <View>
             <Text>All Out Stock</Text>
