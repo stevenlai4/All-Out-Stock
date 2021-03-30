c83ac245e2d09131826371de1f40dabe3b20e667
Steven <clai86@my.bcit.ca>
2021-03-30 15:33:50 -0700

    Detail page layout added

--- all-out-stock/src/screens/StockDetailScreen/StockDetailScreen.js
+++ all-out-stock/src/screens/StockDetailScreen/StockDetailScreen.js
@@ -2,11 +2,28 @@ import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 
 export default function StockDetailScreen({ route }) {
+    const [stock, setStock] = useState(route.params);
+
     return (
         <View>
             <Text>All Out Stock</Text>
-            <Text>{route.params.company}</Text>
-            <Text>{}</Text>
+            <Text>${stock.c}</Text>
+            <View>
+                <Text>Open</Text>
+                <Text>${stock.o}</Text>
+            </View>
+            <View>
+                <Text>High</Text>
+                <Text>${stock.h}</Text>
+            </View>
+            <View>
+                <Text>Low</Text>
+                <Text>${stock.l}</Text>
+            </View>
+            <View>
+                <Text>Previous Closing</Text>
+                <Text>${stock.p}</Text>
+            </View>
         </View>
     );
 }
