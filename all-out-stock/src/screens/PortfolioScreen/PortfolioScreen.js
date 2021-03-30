f22be7f61c7e908a2f8e519b2d16d9669a193004
Shenxuan Wang <swang282@my.bcit.ca>
2021-03-30 14:09:37 -0700

    move stock page navigation to portfolio page

--- all-out-stock/src/screens/PortfolioScreen/PortfolioScreen.js
+++ all-out-stock/src/screens/PortfolioScreen/PortfolioScreen.js
@@ -35,6 +35,9 @@ export default function PortfolioScreen() {
                     </TouchableOpacity>
                 )}
             />
+            <TouchableOpacity onPress={()=>{props.navigation.navigate('Stock')}}>
+                <Text>Click here for Stocks</Text>
+            </TouchableOpacity>
         </View>
     );
 }
