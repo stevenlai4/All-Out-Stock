46ba515d6aaaebabf8ab1d886b4339f7a80ec9dd
abhishekpundir789 <apundir@algomau.ca>
2021-03-31 01:45:19 -0700

    update commit with Evil Icon and adding Drawer Screen

--- all-out-stock/src/screens/NavigatinScreen/navigationScreen.js
+++ all-out-stock/src/screens/NavigatinScreen/navigationScreen.js
@@ -12,3 +12,29 @@ export default function DrawerNavigator() {
         </Drawer.Navigator>
     )
 }
+
+import { createStackNavigator } from '@react-navigation/stack';
+import PortfolioScreen from '../PortfolioScreen/PortfolioScreen';
+import MenuIcon from './MenuIcon';
+
+const PortfolioStack = createStackNavigator()
+function PortfolioScreenNavigator() {
+    return(
+        <PortfolioStack.Navigator>
+            <PortfolioStack.Screen name = "Portfolio" component={PortfolioScreen} options = {{headerTitle: 'User Portfolio', headerLeft: () => <MenuIcon />}}
+            />
+        </PortfolioStack.Navigator>
+    )
+}
+
+import StockScreen from '../StockScreen/StockScreen';
+
+const StockScreenStack = createStackNavigator()
+function StockScreenNavigator() {
+    return(
+        <StockScreenStack.Navigator>
+            <StockScreenStack.Screen name ="Stock" component={StockScreen} options = {{headerTitle: 'Stock Purchase'}}
+            />
+        </StockScreenStack.Navigator>
+    )
+}
