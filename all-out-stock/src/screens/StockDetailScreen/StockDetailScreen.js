107eda5c8827438519c46fe1d83f1f08e1ded5a2
Steven <clai86@my.bcit.ca>
2021-03-30 23:16:56 -0700

    Stock detail page created

--- all-out-stock/src/screens/StockDetailScreen/StockDetailScreen.js
+++ all-out-stock/src/screens/StockDetailScreen/StockDetailScreen.js
@@ -1,28 +1,51 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
+import styles from './styles';
 
-export default function StockDetailScreen({ route }) {
-    const [stock, setStock] = useState(route.params);
+export default function StockDetailScreen(porps) {
+    const [name, setName] = useState('Apple Inc.');
+    const [quote, setQuote] = useState({
+        c: 261.74,
+        h: 263.31,
+        l: 260.68,
+        o: 261.07,
+        pc: 259.45,
+        t: 1582641000,
+    });
 
     return (
-        <View>
-            <Text>All Out Stock</Text>
-            <Text>${stock.c}</Text>
-            <View>
-                <Text>Open</Text>
-                <Text>${stock.o}</Text>
+        <View style={styles.root}>
+            <Text style={[styles.logo, styles.centerText]}>All Out Stock</Text>
+            <Text style={[styles.name, styles.centerText]}>{name}</Text>
+            <Text style={[styles.currentPrice, styles.centerText]}>
+                ${quote.c}
+            </Text>
+            <View style={styles.cell}>
+                <Text style={styles.cellText}>Open</Text>
+                <Text style={styles.cellText}>${quote.o}</Text>
             </View>
-            <View>
-                <Text>High</Text>
-                <Text>${stock.h}</Text>
+            <View style={styles.cell}>
+                <Text style={styles.cellText}>High</Text>
+                <Text style={styles.cellText}>${quote.h}</Text>
             </View>
-            <View>
-                <Text>Low</Text>
-                <Text>${stock.l}</Text>
+            <View style={styles.cell}>
+                <Text style={styles.cellText}>Low</Text>
+                <Text style={styles.cellText}>${quote.l}</Text>
             </View>
-            <View>
-                <Text>Previous Closing</Text>
-                <Text>${stock.p}</Text>
+            <View style={styles.cell}>
+                <Text style={styles.cellText}>Previous Closing</Text>
+                <Text style={styles.cellText}>${quote.pc}</Text>
+            </View>
+            <TouchableOpacity>
+                <Text style={styles.refresh}>Refresh</Text>
+            </TouchableOpacity>
+            <View style={styles.btnContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.btnText}>Buy</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.btnText}>Sell</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
