01485a6e291e6d12471b9d65a5683f84e0850409
Shenxuan Wang <swang282@my.bcit.ca>
2021-04-05 11:15:14 -0700

    apply sell function

--- all-out-stock/src/screens/TransactionScreen/TransactionScreen.js
+++ all-out-stock/src/screens/TransactionScreen/TransactionScreen.js
@@ -15,9 +15,20 @@ export default function TransactionScreen(props) {
     var totalShare = 0
     var totalPrice = 0
 
-    const handleTransaction = async () => {
+    const handleBuyTransaction = async () => {
         
         await db.collection("users").doc(uid).collection("transaction").add({name: stock.name, price: stock.quote.c, share: parseInt(share)});
+        const transactions = await db.collection("users").doc(uid).collection("transaction").get({name: stock.name})
+        transactions.forEach((transaction) => {
+            totalShare += transaction.data().share
+            totalPrice += transaction.data().price * transaction.data().share
+        })
+        setAvgPrice(totalPrice/totalShare)
+    }
+
+    const handleSellTransaction = async () => {
+        
+        await db.collection("users").doc(uid).collection("transaction").add({name: stock.name, price: stock.quote.c, share: parseInt(-share)});
         const transactions = await db.collection("users").doc(uid).collection("transaction").get({name: stock.name})
         transactions.forEach((transaction) => {
             totalShare += transaction.data().share
@@ -32,7 +43,7 @@ export default function TransactionScreen(props) {
 
             <Text> How many shares of {stock.name} do you want to buy?</Text>
             
-            <TextInput placeholder="0" keyboardType="numeric" onChangeText={(text) => setShare(text)}> </TextInput>
+            <TextInput style={styles.text} placeholder="0" keyboardType="numeric" onChangeText={(text) => setShare(text)}> </TextInput>
             <Text>
                 {stock.name} Price: ${stock.quote.c}
             </Text>
@@ -47,10 +58,10 @@ export default function TransactionScreen(props) {
             <View style={styles.btnContainer}>
                 {
                     isBuying &&
-                    <TouchableOpacity style={styles.button} onPress={()=>{handleTransaction()}}>
+                    <TouchableOpacity style={styles.button} onPress={()=>{handleBuyTransaction()}}>
                         <Text style={styles.btnText}>Buy</Text>
                     </TouchableOpacity> ||
-                    <TouchableOpacity style={styles.button} onPress={()=>{handleTransaction()}}>
+                    <TouchableOpacity style={styles.button} onPress={()=>{handleSellTransaction()}}>
                     <Text style={styles.btnText}>Sell</Text>
                 </TouchableOpacity>
                 }
